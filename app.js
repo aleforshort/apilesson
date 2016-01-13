80a4804840627d787ea1a73f9be7c9ec2a1b32ad
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2016-01-13 06:36:23 -0500

    update

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 $(function(){});
 
-$("button").click(function(){
-	var userSearch = $("input").val();
 
-	if(userSearch!=""){
-		userQuery;
-
-	}
-});
-
-var userQuery = $(function(){
-
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    console.log(data);
+$(function(){
+	$("button").on("click",".submit",function(event){
+	event.preventDefault();
+	var userInput = $("query").val();
+  	$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+	 
+	 showResults(data.Search);
+    });
   });
 });
+
+
+function showResults(results){
+	var html = "";
+	// var inhtml = $("#search-results").find("p");
+	$.each(results, function(index,value){
+		html += '<p>' + value.Title + '</p>';
+		// inhtml.append(value.Title);
+		console.log(value.Title);
+	});
+	  $('#search-results').html(html);
+}
+
